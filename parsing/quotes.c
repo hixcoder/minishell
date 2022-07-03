2e06b19ca18963406af8f4f64b737d4358121c93
Hamza Boumahdi <hboumahd@e2r2p12.1337.ma>
2022-07-03 19:02:42 +0100

    understanding the concept of lexer

--- parsing/quotes.c
+++ parsing/quotes.c
@@ -6,34 +6,34 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 12:14:45 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/07/02 13:32:47 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/07/03 11:11:44 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int    ft_quotes_handler(char *s)
-{
-    int i;
+// int    ft_quotes_handler(char *s)
+// {
+//     int i;
     
-    i = -1;
-    while(s[++i])
-    {
-        if (s[i] == q)
-            return (i);
-    }
-    return (-1);
-}
+//     i = -1;
+//     while(s[++i])
+//     {
+//         if (s[i] == q)
+//             return (i);
+//     }
+//     return (-1);
+// }
 
-int    ft_quotes_checker(char q, char *start)
-{
-    int i;
+// int    ft_quotes_checker(char q, char *start)
+// {
+//     int i;
     
-    i = -1;
-    while(start[++i])
-    {
-        if (start[i] == q)
-            return (i);
-    }
-    return (-1);
-}
+//     i = -1;
+//     while(start[++i])
+//     {
+//         if (start[i] == q)
+//             return (i);
+//     }
+//     return (-1);
+// }
