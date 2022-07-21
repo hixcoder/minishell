0aadbb68b11a99c452d6a3cefa5054547080f8a1
Hamza Boumahdi <hboumahd@e3r6p1.1337.ma>
2022-07-21 12:20:04 +0100

    I split args by pipe and split cmds and thier attributes

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 13:12:00 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/07/02 13:13:03 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/07/21 12:13:35 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,19 @@
 # include <readline/history.h>
 # include "./libft/libft.h"
 
+// cmd : command
+// atr : atributes
 typedef struct s_command
 {
-    char    *cmd;
-    char    *atr;
-    char    *input;
+	char    *cmd;
+	char    **atr;
 }   t_command;
 
 typedef struct s_data
 {
-    char    *args;
-    char    **cmds;
+	char        *args;
+	t_command   *cmds;
+	int			*cmds_len;
 }   t_data;
 
 void    ft_readline(t_data *data);
--- parsing/parse.c
+++ parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 13:19:31 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/07/02 13:52:56 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/07/21 12:15:05 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,45 @@
 
 void    ft_readline(t_data *data)
 {
-    while (1)
-    {
-        data->args = readline("Minishell ++> ");
-        // need to check the quotes rules here
+	char		**cmdsTmp;
+	int j;
+	int i;
+	
+	while (1)
+	{
+		data->args = readline("Minishell ++> ");
+		if (ft_strlen(data->args) > 0)
+			add_history(data->args);
 
-        // trim and split the args
-        data->cmds = ft_split(ft_strtrim(data->args, " "), '|');
-        printf("args : %s\n", data->args);
-        int i = -1;
+		// trim and split the input by '|'
+		cmdsTmp = ft_split(ft_strtrim(data->args, " "), '|');
+		printf("args : %s\n", data->args);
 
-        // trim the commands
-        while (data->cmds[++i])
-        {
-            data->cmds[i] = ft_strtrim(data->cmds[i], " ");
-            printf("cmds[%d] : %s\n", i, data->cmds[i]);
-        }
-        
-        if (ft_strlen(data->args) > 0)
-            add_history(data->args);
-    }
+		// trim the commands and split them by " " then fill the values of data->cmds
+		i = -1;
+		while (cmdsTmp[++i])
+		{
+			printf("cmdsTmp[%d] : %s\n", i, cmdsTmp[i]);
+			data->cmds[i].atr = ft_split(ft_strtrim(cmdsTmp[i], " "), ' ');
+			j = -1;
+			while (data->cmds[i].atr[++j])
+			{
+				if (j == 0)
+					data->cmds[i].cmd = data->cmds[i].atr[j];
+			}
+		}
+		data->cmds_len = i;
+		data->cmds[j].atr = &(data->cmds[j].atr[1]);
+
+		// here I print the values of data->cmds
+		j = -1;
+		while (++j < i)
+		{
+			printf("data->cmds[%d].cmd : %s\n", j, data->cmds[j].cmd);
+			int k = -1;
+			while (data->cmds[j].atr[++k])
+				printf("data->cmds[%d].atr[%d] : %s\n", j, k, data->cmds[j].atr[k]);
+			
+		}
+	}
 }
