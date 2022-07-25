7b06ce3b7ded2e2c40fb76461fdca5fdbf2a0a8d
Hamza Boumahdi <hboumahd@e3r3p15.1337.ma>
2022-07-25 11:13:52 +0100

    how to use execve() function in c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 13:11:01 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/07/23 10:02:48 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/07/25 11:04:15 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ int main(int ac, char **av, char **env)
    (void) env;
    // for(int i = 0; env[i]; i++)
    //    printf("%s\n", env[i]);
+   printf("Main program started\n");
+  char* argv[] = {"ls", NULL };
+  char* envp[] = { "some", "environment", NULL };
+  if (execve("/bin/ls", argv, envp) == -1)
+    perror("Could not execve");
    ft_readline(&data);
    return (0);
 }
